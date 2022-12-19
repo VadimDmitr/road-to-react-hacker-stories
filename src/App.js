dd8cad1c5a39a56795165e1974560fd6a62125f4
Vadim <vadzzimluch@gmail.com>
2022-12-19 11:54:35 -0500

    Add props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import * as React from 'react';
- 
-const list = [
+
+function List(props) {
+  return (
+    <ul>
+      {props.list.map(function(item) {
+        return (<Item item={item} />);
+      })}
+    </ul>
+  );
+}
+
+const Item = (props) => {
+  const item = props.item;
+
+  return (
+    <li key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+  );
+}
+
+function Search() {
+  const handleChange = (event) => {
+    console.log(event);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+}
+
+function App() {
+  
+const stories = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
@@ -8,7 +48,7 @@ const list = [
     num_comments: 3,
     points: 4,
     objectID: 0,
-  }, 
+  },
   {
     title: 'Redux',
     url: 'https://redux.js.org/',
@@ -16,46 +56,17 @@ const list = [
     num_comments: 2,
     points: 5,
     objectID: 1,
-  }, 
+  },
 ];
 
-function List(){
   return (
-    <ul>
-      {list.map(function(item) {
-          return (
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                 </span> 
-                 <span>{item.author}</span>
-                 <span>{item.num_comments}</span>
-                 <span>{item.points}</span>
-             </li>
-              );
-          })}
-    </ul>   
-  );
-}
-
-function Search() {
-  return (
-   <div>
-     <label htmlFor='search'>Search: </label>
-           <input id="search" type="text" />
-   </div>        
-  );
-}
-
-function App() {
-   return (
-     <div>
+    <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 }
-
+ 
 export default App;
