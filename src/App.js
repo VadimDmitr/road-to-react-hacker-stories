80342bfb5486fec47ff7cfa16bda1b56008e432c
Vadim <vadzzimluch@gmail.com>
2023-02-06 17:14:57 -0500

    Third-Party Libraries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -64,13 +65,14 @@ const App = () => {
 
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-
-    fetch(url)
-      .then((response) => response.json())
+    
+    axios
+    get(url)
+      //.then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
